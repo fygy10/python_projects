5a17f36f226c48f65f682baa6e886b0718e690e8
fygy10 <niko.timothyl@gmail.com>
2023-05-22 09:29:14 -0400

    adding FizzBuzz and a prime number function tool

---
+++ prime_number_function.py
@@ -0,0 +1,12 @@
+min_range = input("Please enter a minimum range of integers: ")
+min_range = int(min_range)
+max_range = input("Please enter a maximum range of integers: ")
+max_range = int(max_range)
+
+for i in range(min_range, max_range):
+    for n in range(2, i):
+        if i % n == 0:
+            print(f"{i} is not a prime number; {i} equals {n} * {i//n}")
+            break
+    else:
+        print(f"{i} is a prime number.")
